1d33bfa5eef922a07bd91b6d39765a93ea985bd9
ziko-ZR1 <zakariya-khadiri@hotmail.fr>
2020-06-03 18:19:48 +0100

    download video from url

--- pfe/translation/urls.py
+++ pfe/translation/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 from . import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 app_name = 'translation'
 urlpatterns = [
     path('', views.index,name='index'),
     path('start-now/', views.play_video,name='startnow'),
 ]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- pfe/translation/views.py
+++ pfe/translation/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from django.core.files.storage import FileSystemStorage
+import requests,os
 
 def index(request):
     return render(request,'translation/index.html', {'nbar': 'home'})
@@ -10,4 +11,22 @@ def error_404(request, exception):
     return render(request,'translation/404.html', status = 404)
 
 def play_video(request):
+    if request.method == 'POST' and 'Select' in request.POST:
+        print (request)
+        video = request.FILES['myVideo']
+        fs = FileSystemStorage()
+        fs.save(video.name,video)
+    elif request.method=='POST' and 'Search' in request.POST:
+        url = request.POST.get('link')
+        local_filename = url.split('/')[-1]
+        r = requests.get(url)
+        with open('{}/{}'.format(os.path.abspath('translation/media'),local_filename), 'wb') as f:
+            for chunk in r.iter_content(chunk_size=1024): 
+                if chunk: 
+                    f.write(chunk)
+    
     return render(request,'translation/blog-single.html')
+
+
+
+#print(os.path.abspath('translation/media'))
