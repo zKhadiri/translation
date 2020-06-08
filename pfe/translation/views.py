19db5255c48b83dc9866d31a6677d1274ba69ec7
ziko-ZR1 <zakariya-khadiri@hotmail.fr>
2020-06-08 12:57:07 +0100

    download youtube video highest resolution

--- pfe/translation/views.py
+++ pfe/translation/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.core.files.storage import FileSystemStorage
-import requests,os,re,pytube
+import requests,os,re,pytube,sys
 from clint.textui import progress
 from time import sleep
+
+
+
 
 def index(request):
     return render(request,'translation/index.html', {'nbar': 'home'})
@@ -13,6 +16,7 @@ def error_404(request, exception):
     return render(request,'translation/404.html', status = 404)
 
 def play_video(request):
+    global file_size
     if request.method == 'POST' and 'Select' in request.POST:
         print (request)
         video = request.FILES['myVideo']
@@ -21,15 +25,15 @@ def play_video(request):
     elif request.method=='POST' and 'Search' in request.POST:
         url = request.POST.get('link')
         if re.match(r'^(https?\:\/\/)?(www\.youtube\.com|youtu\.?be)\/.+$',url):
-            video = pytube.YouTube(url)
-            video.streams.all()
-            for tag in video.streams:
-                print(tag)
-            video.streams.get_by_itag(22).download(os.path.abspath('translation/media')) 
+            video = pytube.YouTube(url, on_progress_callback=progress_function)
+            video_type = video.streams.filter(progressive = True, file_extension = "mp4").first()
+            file_size = video_type.filesize
+            video.streams.get_highest_resolution().download(os.path.abspath('translation/media'))
         else:
             local_filename = url.split('/')[-1]
             r = requests.get(url,stream=True)
             total_length = int(r.headers.get('content-length'))
+            
             with open('{}/{}'.format(os.path.abspath('translation/media'),local_filename), 'wb') as f:
                 for chunk in progress.bar(r.iter_content(chunk_size=1024), expected_size=(total_length/1024) + 1): 
                     if chunk: 
@@ -38,3 +42,6 @@ def play_video(request):
             sleep(1)
     return render(request,'translation/blog-single.html')
 
+def progress_function(stream, chunk, file_handle, bytes_remaining):
+    print(round((1-bytes_remaining/file_size.filesize)*100, 3), '% done...')
+    
