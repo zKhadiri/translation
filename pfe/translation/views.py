0531a11ebfaef59a6563f07d279e78c666a0862c
ziko-ZR1 <zakariya-khadiri@hotmail.fr>
2020-06-12 00:51:48 +0100

    update

--- pfe/translation/views.py
+++ pfe/translation/views.py
@@ -1,12 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.core.files.storage import FileSystemStorage
-import requests,os,re,pytube,sys,json
+import requests,os,re,pytube,sys,json,mimetypes
 import pytube
 from time import sleep
 from django.views.decorators.csrf import csrf_exempt
 
-video_format=['mp4','webm','ogv','3gp','mkv']
 
 def index(request):
     return render(request,'translation/index.html', {'nbar': 'home'})
@@ -30,18 +29,20 @@ def play_video(request):
             fs.save(video.name,video)
             return HttpResponse(json.dumps({'message': 'ok',}),content_type="application/json")
         elif url != "" and url != None:
-            if re.match(r'^(https?\:\/\/)?(www\.youtube\.com|youtu\.?be)\/.+$',url) and url != "":
-                video = pytube.YouTube(url)
-                video.streams.get_highest_resolution().download(os.path.abspath('translation/media'))
-            elif [frm for frm in video_format if frm in url]:#''.join(video_format) in url:
-                local_filename = url.split('/')[-1]
-                r = requests.get(url,stream=True)
-                with open('{}/{}'.format(os.path.abspath('translation/media'),local_filename), 'wb') as f:
-                    for chunk in r.iter_content(chunk_size=1024):
-                        if chunk:
-                            f.write(chunk)
-            return HttpResponse(json.dumps({'message': 'ok',}),content_type="application/json")
+            try:
+                if re.match(r'^(https?\:\/\/)?(www\.youtube\.com|youtu\.?be)\/.+$',url):
+                    video = pytube.YouTube(url)
+                    video.streams.get_highest_resolution().download(os.path.abspath('translation/media'))
+                elif mimetypes.MimeTypes().guess_type(url)[0].startswith("video"):
+                    local_filename = url.split('/')[-1]
+                    r = requests.get(url,stream=True)
+                    with open('{}/{}'.format(os.path.abspath('translation/media'),local_filename), 'wb') as f:
+                        for chunk in r.iter_content(chunk_size=1024):
+                            if chunk:
+                                f.write(chunk)
+                return HttpResponse(json.dumps({'message': 'ok',}),content_type="application/json")
+            except Exception:
+                return HttpResponse(json.dumps({'message': 'not valid',}),content_type="application/json")
         elif url =='' and not "myVideo" in request.FILES:
             return HttpResponse(json.dumps({'message': 'empty',}),content_type="application/json")
-            
     return render(request,'translation/blog-single.html')
