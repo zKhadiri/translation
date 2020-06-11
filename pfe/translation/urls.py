00f9f4b071fc76e2007e406009ba2870265dd1ca
ziko-ZR1 <zakariya-khadiri@hotmail.fr>
2020-06-11 21:08:59 +0100

    update

--- pfe/translation/urls.py
+++ pfe/translation/urls.py
@@ -7,6 +7,8 @@ app_name = 'translation'
 urlpatterns = [
     path('', views.index,name='index'),
     path('start-now/', views.play_video,name='startnow'),
+    path('creat-script/', views.creation_script,name='creat-script'),
+    path('generate-script/', views.generer_script,name='generate-script'),
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- pfe/translation/views.py
+++ pfe/translation/views.py
@@ -14,6 +14,11 @@ def index(request):
 def error_404(request, exception):
     return render(request,'translation/404.html', status = 404)
 
+def creation_script(request):
+    return render(request,'translation/creation_scripte.html')
+
+def generer_script(request):
+    return render(request,'translation/generer_script.html')
 
 @csrf_exempt
 def play_video(request):
@@ -24,7 +29,7 @@ def play_video(request):
             fs = FileSystemStorage()
             fs.save(video.name,video)
             return HttpResponse(json.dumps({'message': 'ok',}),content_type="application/json")
-        if url != "" and url != None:
+        elif url != "" and url != None:
             if re.match(r'^(https?\:\/\/)?(www\.youtube\.com|youtu\.?be)\/.+$',url) and url != "":
                 video = pytube.YouTube(url)
                 video.streams.get_highest_resolution().download(os.path.abspath('translation/media'))
@@ -40,6 +45,3 @@ def play_video(request):
             return HttpResponse(json.dumps({'message': 'empty',}),content_type="application/json")
             
     return render(request,'translation/blog-single.html')
-
-
-    
